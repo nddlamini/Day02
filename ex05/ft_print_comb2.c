0861891e2f2b7a1f6c23888d28e63ae5274c0c21
Ndumiso Dlamini <nddlamin@c3r5s4.wethinkcode.co.za>
2020-06-19 16:13:41 +0200

    all

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nddlamin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*             :                                   +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 15:21:55 by nddlamin          #+#    #+#             */
+/*   Updated: 2020/06/19 15:36:19 by nddlamin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+void	printdigits(int n)
+{
+	if (n < 10)
+	{
+		ft_putchar('0');
+		ft_putchar(n + '0');
+	}
+	else
+	{
+		
+		ft_putchar(n / 10 + '0');
+		ft_putchar(n % 10 + '0');
+	}
+}
+
+void	printnum(int c1, int c2)
+{
+	printdigits(c1);
+	ft_putchar(' ');
+	printdigits(c2);
+	if (c1 != 98 || c2 != 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 99)
+	{
+		j = i;
+		while (j <= 99)
+		{
+			if (i != j)
+				printnum(i, j);
+			++j;
+		}
+		++i;
+	}
+}
